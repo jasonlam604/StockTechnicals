adbde8f718e027e22db6a469a40ab327eae7e3c2
Jason Lam <jasonlam604@gmail.com>
2016-10-23 20:09:07 -0700

    Remove Indicator interface, not currently used

--- src/com/jasonlam604/stocktechnicals/indicators/Indicator.java
+++
@@ -1,7 +0,0 @@
-package com.jasonlam604.stocktechnicals.indicators;
-
-public interface Indicator {
-
-	public double execute(double high, double low, double close);
-	public double[] executeSet(double[] high, double[] low, double[] close) throws Exception;
-}
