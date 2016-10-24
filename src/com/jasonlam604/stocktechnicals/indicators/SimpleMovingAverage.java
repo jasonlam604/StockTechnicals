fac21b26635afeb0c4e25c4070157d9b7d6df827
Jason Lam <jasonlam604@gmail.com>
2016-10-23 20:01:45 -0700

    Minor Formatting

--- src/com/jasonlam604/stocktechnicals/indicators/SimpleMovingAverage.java
+++ src/com/jasonlam604/stocktechnicals/indicators/SimpleMovingAverage.java
@@ -7,35 +7,54 @@ import com.jasonlam604.stocktechnicals.util.NumberHelper;
 public class SimpleMovingAverage {
 
 	/**
-	 *  Simple Moving Average is formed by computing the average price of a security over 
-	 *  a specific number of periods. 
-	 *  
-	 *  Calculation starts from index 0 to index n assuming there is enough periods
+	 * Simple Moving Average is formed by computing the average price of a
+	 * security over a specific number of periods.
 	 * 
-	 * @param price double array of values
-	 * @param period SMA period to be calculated over
+	 * Calculation starts from index 0 to index n assuming there is enough
+	 * periods
+	 * 
+	 * @param price
+	 *            double array of values
+	 * @param period
+	 *            SMA period to be calculated over
 	 * @throws Exception
 	 */
-	public double[] execute(double[] price,int period) throws Exception {
-		
+	public double[] execute(double[] price, int period) throws Exception {
+
 		// ie: if you want 50 SMA then you need 50 data points
-		if(price.length < period)
+		if (price.length < period)
 			throw new Exception("Not enough data point, given data size less then the indicated period");
-		
+
 		double[] results = new double[price.length];
-		
+
 		int maxLength = price.length - period;
-		for(int i=0; i<= maxLength; i++) {
-			results[i] = NumberHelper.roundTwoDecimals((Arrays.stream(Arrays.copyOfRange(price, i, (i+period))).sum()) /  period);
+		for (int i = 0; i <= maxLength; i++) {
+			results[i] = NumberHelper
+					.roundTwoDecimals((Arrays.stream(Arrays.copyOfRange(price, i, (i + period))).sum()) / period);
 		}
-		
+
 		return results;
 	}
-	
+
+	/**
+	 * 
+	 * SMA 50
+	 * 
+	 * @param price
+	 * @return
+	 * @throws Exception
+	 */
 	public double[] sma50(double[] price) throws Exception {
 		return this.execute(price, 50);
 	}
-	
+
+	/**
+	 * SMA 200
+	 * 
+	 * @param price
+	 * @return
+	 * @throws Exception
+	 */
 	public double[] sma200(double[] price) throws Exception {
 		return this.execute(price, 200);
 	}
--- src/com/jasonlam604/stocktechnicals/indicators/TypicalPrice.java
+++ src/com/jasonlam604/stocktechnicals/indicators/TypicalPrice.java
@@ -6,35 +6,35 @@ import com.jasonlam604.stocktechnicals.util.NumberHelper;
  * 
  * Typical Price
  *
- * The Typical Price indicator is simply an average of each day's price. 
- * The Median Price and Weighted Close are similar indicators. The Typical Price 
- * indicator provides a simple, single-line plot of the day's average price. 
- * Some investors use the Typical Price rather than the closing price when 
- * creating moving average penetration systems. 
+ * The Typical Price indicator is simply an average of each day's price. The
+ * Median Price and Weighted Close are similar indicators. The Typical Price
+ * indicator provides a simple, single-line plot of the day's average price.
+ * Some investors use the Typical Price rather than the closing price when
+ * creating moving average penetration systems.
  *
- *  TP= (high+low+close) / 3
+ * TP= (high+low+close) / 3
  *
  */
-public class TypicalPrice implements Indicator {
+public class TypicalPrice {
 
 	public double[] executeSet(double[] high, double[] low, double[] close) throws Exception {
-		
+
 		double[] results = new double[high.length];
-		
-		if(high.length == low.length && high.length == close.length) {
-			
-			for(int i=0; i<high.length; i++) {
+
+		if (high.length == low.length && high.length == close.length) {
+
+			for (int i = 0; i < high.length; i++) {
 				results[i] = this.execute(high[i], low[i], close[i]);
 			}
-			
+
 		} else {
 			throw new Exception("Size of high, low and close are not the same");
 		}
-		
+
 		return results;
-		
+
 	}
-	
+
 	/**
 	 * Typical Price
 	 * 
@@ -44,9 +44,6 @@ public class TypicalPrice implements Indicator {
 	 * @return
 	 */
 	public double execute(double high, double low, double close) {
-		//return (high + low + close)/3;
-		return NumberHelper.roundTwoDecimals((high + low + close)/3);
-		//return Math.round(  ((high + low + close)/3.0)*100   ) / 100;
+		return NumberHelper.roundTwoDecimals((high + low + close) / 3);
 	}
 }
-
