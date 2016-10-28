a736695c637e6e312de928f21bb67d5952713e74
Jason Lam <jasonlam604@gmail.com>
2016-10-27 20:54:35 -0700

    Fix formatting

--- test/com/jasonlam604/stocktechnicals/indicators/SimpleMovingAverageTest.java
+++ test/com/jasonlam604/stocktechnicals/indicators/SimpleMovingAverageTest.java
@@ -7,46 +7,47 @@ import org.junit.Assert;
 
 public class SimpleMovingAverageTest {
 
-	
-    @Test
-    public void calcSimpleMovingAverage1() {
-    	SimpleMovingAverage sma = new SimpleMovingAverage();
-    	try {
-			 double[] results = sma.execute(new double []{17,16,15,14,13,12,11}, 5);
-			
-			 Assert.assertEquals(15.0,results[0],0);
-			 Assert.assertEquals(14.0,results[1],0);
-			 Assert.assertEquals(13.0,results[2],0);
-			
+	@Test
+	public void calcSimpleMovingAverage1() {
+		SimpleMovingAverage sma = new SimpleMovingAverage();
+		try {
+			double[] results = sma.execute(new double[] { 17, 16, 15, 14, 13, 12, 11 }, 5);
+
+			Assert.assertEquals(15.0, results[0], 0);
+			Assert.assertEquals(14.0, results[1], 0);
+			Assert.assertEquals(13.0, results[2], 0);
+
 		} catch (Exception e) {
-			
+
 		}
-    }
-    
-    @Test
-    public void calcSimpleMovingAverage2() {
-    	SimpleMovingAverage sma = new SimpleMovingAverage();
-    	try {
-			 double[] results = sma.execute(new double []{22.27,22.19,22.08,22.17,22.18,22.13,22.23,22.43,22.24,22.29,22.15,22.39,22.38,22.61,23.36,24.05,23.75,23.83,23.95,23.63,23.82,23.87,23.65,23.19,23.10,23.33,22.68,23.10,22.40,22.17}, 10);
-			
-			 Assert.assertEquals(22.22,results[0],0);
-			 Assert.assertEquals(22.21,results[1],0);
-			 Assert.assertEquals(22.23,results[2],0);
-			 Assert.assertEquals(22.26,results[3],0);
-			 Assert.assertEquals(22.30,results[4],0);
-			 Assert.assertEquals(22.42,results[5],0);
-			 Assert.assertEquals(22.61,results[6],0);
-			 Assert.assertEquals(22.77,results[7],0);
-			 Assert.assertEquals(22.90,results[8],0);
-			 Assert.assertEquals(23.08,results[9],0);
-			 Assert.assertEquals(23.21,results[10],0);
-			 Assert.assertEquals(23.38,results[11],0);
-			 Assert.assertEquals(23.52,results[12],0);
-			 Assert.assertEquals(23.65,results[13],0);
-			 Assert.assertEquals(23.71,results[14],0);
-			
+	}
+
+	@Test
+	public void calcSimpleMovingAverage2() {
+		SimpleMovingAverage sma = new SimpleMovingAverage();
+		try {
+			double[] results = sma.execute(new double[] { 22.27, 22.19, 22.08, 22.17, 22.18, 22.13, 22.23, 22.43, 22.24,
+					22.29, 22.15, 22.39, 22.38, 22.61, 23.36, 24.05, 23.75, 23.83, 23.95, 23.63, 23.82, 23.87, 23.65,
+					23.19, 23.10, 23.33, 22.68, 23.10, 22.40, 22.17 }, 10);
+
+			Assert.assertEquals(22.22, results[0], 0);
+			Assert.assertEquals(22.21, results[1], 0);
+			Assert.assertEquals(22.23, results[2], 0);
+			Assert.assertEquals(22.26, results[3], 0);
+			Assert.assertEquals(22.30, results[4], 0);
+			Assert.assertEquals(22.42, results[5], 0);
+			Assert.assertEquals(22.61, results[6], 0);
+			Assert.assertEquals(22.77, results[7], 0);
+			Assert.assertEquals(22.90, results[8], 0);
+			Assert.assertEquals(23.08, results[9], 0);
+			Assert.assertEquals(23.21, results[10], 0);
+			Assert.assertEquals(23.38, results[11], 0);
+			Assert.assertEquals(23.52, results[12], 0);
+			Assert.assertEquals(23.65, results[13], 0);
+			Assert.assertEquals(23.71, results[14], 0);
+
 		} catch (Exception e) {
-			
+
 		}
-    }
+	}
 }
--- test/com/jasonlam604/stocktechnicals/indicators/TypicalPriceTest.java
+++ test/com/jasonlam604/stocktechnicals/indicators/TypicalPriceTest.java
@@ -5,36 +5,35 @@ import com.jasonlam604.stocktechnicals.indicators.TypicalPrice;
 import org.junit.Test;
 import org.junit.Assert;
 
-
 public class TypicalPriceTest {
-	
-    @Test
-    public void calcTypicalPrice() {
-    	TypicalPrice tp = new TypicalPrice();
-    	Assert.assertEquals(23.98,tp.execute(24.20, 23.85, 23.89),0);
-    }
-    
-    @Test
-    public void calcTypicalPriceSet() {
-    	
-    	double[] high = {24.20,24.07,24.04,23.87,23.67};
-    	double[] low  = {23.85,23.72,23.64,23.37,23.46};
-    	double[] close = {23.89,23.95,23.67,23.78,23.50};
-    	
-    	TypicalPrice tp = new TypicalPrice();
-    	
-    	double[] results = null;
-		
-    	try {
+
+	@Test
+	public void calcTypicalPrice() {
+		TypicalPrice tp = new TypicalPrice();
+		Assert.assertEquals(23.98, tp.execute(24.20, 23.85, 23.89), 0);
+	}
+
+	@Test
+	public void calcTypicalPriceSet() {
+
+		double[] high = { 24.20, 24.07, 24.04, 23.87, 23.67 };
+		double[] low = { 23.85, 23.72, 23.64, 23.37, 23.46 };
+		double[] close = { 23.89, 23.95, 23.67, 23.78, 23.50 };
+
+		TypicalPrice tp = new TypicalPrice();
+
+		double[] results = null;
+
+		try {
 			results = tp.executeSet(high, low, close);
 		} catch (Exception e) {
-			
+
 		}
-    	
-    	Assert.assertEquals(23.98,results[0],0);
-    	Assert.assertEquals(23.91,results[1],0);
-    	Assert.assertEquals(23.78,results[2],0);
-    	Assert.assertEquals(23.67,results[3],0);
-    	Assert.assertEquals(23.54,results[4],0);
-    }
+
+		Assert.assertEquals(23.98, results[0], 0);
+		Assert.assertEquals(23.91, results[1], 0);
+		Assert.assertEquals(23.78, results[2], 0);
+		Assert.assertEquals(23.67, results[3], 0);
+		Assert.assertEquals(23.54, results[4], 0);
+	}
 }
