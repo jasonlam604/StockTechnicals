691a77967e2c26ede8623cc2063a8c771f6a84c7
Jason Lam <jasonlam604@gmail.com>
2016-11-06 13:14:22 -0800

    Just a test stub

---
+++ test/com/jasonlam604/stocktechnicals/overlays/PivotPointsTest.java
@@ -0,0 +1,21 @@
+package com.jasonlam604.stocktechnicals.overlays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.jasonlam604.stocktechnicals.overlays.PivotPoints;
+
+public class PivotPointsTest {
+
+    @Test
+    public void calcTypicalPrice() {
+    	
+    	//PivotPoints pivotPoints = new PivotPoints();
+    	//PivotPoints.PivotPoint pp =  pivotPoints.execute(1,1,1);
+    	
+    	// Todo
+    	
+    	Assert.assertTrue(true);
+    }
+	
+}
