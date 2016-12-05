b0610f430567c0ee25dbdf2cd65cd54b83960a30
Jason Lam <jasonlam604@gmail.com>
2016-12-04 17:43:19 -0800

    Add Trend direction change test

--- test/com/jasonlam604/stocktechnicals/indicators/ParabolicSarTest.java
+++ test/com/jasonlam604/stocktechnicals/indicators/ParabolicSarTest.java
@@ -29,7 +29,7 @@ public class ParabolicSarTest {
 		for (int i = 1; i < trendsResults.length; i++) {
 			Assert.assertEquals(1, trendsResults[i], 0);
 		}
-		
+
 		Assert.assertEquals(47.25, psarResults[1], 0);
 		Assert.assertEquals(47.25, psarResults[2], 0);
 		Assert.assertEquals(47.27, psarResults[3], 0);
@@ -71,7 +71,7 @@ public class ParabolicSarTest {
 		for (int i = 1; i < trendsResults.length; i++) {
 			Assert.assertEquals(-1, trendsResults[i], 0);
 		}
-		
+
 		Assert.assertEquals(46.59, psarResults[1], 0);
 		Assert.assertEquals(46.59, psarResults[2], 0);
 		Assert.assertEquals(46.55, psarResults[3], 0);
@@ -87,6 +87,35 @@ public class ParabolicSarTest {
 		Assert.assertEquals(44.18, psarResults[13], 0);
 		Assert.assertEquals(43.84, psarResults[14], 0);
 		Assert.assertEquals(43.56, psarResults[15], 0);
+	}
+
+	/**
+	 * Taho (THO on TSX), Trading days EOD data from Oct 19 2016 to Dec 02, 2016
+	 * 
+	 * Just quick test to see trend changes
+	 */
+	@Test
+	public void testPsarUpAndDownTrend() {
+
+		double[] high = new double[] { 16.14, 15.89, 16.14, 16.27, 16.59, 16.50, 16.15, 16.24, 16.24, 16.76, 16.91,
+				16.17, 15.95, 14.89, 15.06, 15.37, 14.64, 14.5, 13.56, 13.62, 13.21, 13.49, 13.07, 13.16, 13.26, 12.63,
+				12.59, 12.92, 12.9, 12.92, 12.91, 13.07, 13.46 };
+		double[] low = new double[] { 15.62, 15.46, 15.79, 15.65, 15.95, 15.80, 15.61, 15.54, 15.65, 16.20, 15.65,
+				15.58, 14.96, 14.18, 14.27, 14.3, 13.73, 13.12, 12.38, 12.9, 12.62, 12.82, 12.43, 12.75, 12.93, 11.95,
+				12.39, 12.49, 12.53, 12.45, 12.51, 12.57, 12.73 };
+
+		ParabolicSar psar = new ParabolicSar();
+		psar.calculate(high, low);
+
+		double[] trendsResults = psar.getTrends();
+
+		Assert.assertEquals(1, trendsResults[9], 0);
+		Assert.assertEquals(1, trendsResults[10], 0);
+		Assert.assertEquals(-1, trendsResults[11], 0);
+		Assert.assertEquals(-1, trendsResults[30], 0);
+		Assert.assertEquals(1, trendsResults[32], 0);
+		Assert.assertEquals(1, trendsResults[32], 0);
+
 	}
 
 }
